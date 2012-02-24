082a9d3fb7c7fbe058be729f79ae5728852212e1
Eduardo Folly <edufolly@gmail.com>
2012-02-24 21:49:19 -0200

    Projeto

---
+++ src/br/com/oslunaticos/dicionario/Dicionario.java
@@ -0,0 +1,12 @@
+package br.com.oslunaticos.dicionario;
+
+/**
+ *
+ * @author Eduardo Folly
+ */
+public class Dicionario {
+
+    public static void main(String[] args) {
+        System.out.println("Dicionário");
+    }
+}
